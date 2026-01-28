f0f22db5f3650a6b35fda6a6c30760db33e1497d
Gabriel Virrey <gabriel@ailuruslabs.dev>
2026-01-28 14:44:22 +0800

    Use externalized config and switch to YAML format
    This commit leans toward more standard Spring Boot app configuration practices, removing the need for an additional dependency.
    
    This commit also migrates to the YAML format for the application properties file, which is cleaner and easier to extend.

--- src/main/java/com/project/restapi/employeeManagement/SmartEmployeeManagementApiApplication.java
+++ src/main/java/com/project/restapi/employeeManagement/SmartEmployeeManagementApiApplication.java
@@ -1,6 +1,5 @@
 package com.project.restapi.employeeManagement;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,18 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmartEmployeeManagementApiApplication {
 
     public static void main(String[] args) {
-
-        Dotenv dotenv = Dotenv.load();
-
-        // Optionally, set system properties for Spring Boot
-        System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
-        System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
-        System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
-        System.setProperty("server.port", dotenv.get("PORT", "8080")); // default 8080
-
         SpringApplication.run(SmartEmployeeManagementApiApplication.class, args);
-
-
     }
 
 }
