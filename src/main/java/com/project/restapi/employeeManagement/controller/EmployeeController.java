343fa83f78d072214ced71ccc54e18bdfa817414
Gabriel Virrey <gabriel@ailuruslabs.dev>
2026-01-29 00:46:22 +0800

    Remove unnecessary @Autowired on EmployeeController

--- src/main/java/com/project/restapi/employeeManagement/controller/EmployeeController.java
+++ src/main/java/com/project/restapi/employeeManagement/controller/EmployeeController.java
@@ -17,7 +17,6 @@ public class EmployeeController {
 
     private final EmployeeService employeeService;
 
-    @Autowired
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
